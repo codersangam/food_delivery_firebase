def51ab9ca8555ffcd51a8622610a6e592e485ee
Sangam Singh <hritriik@gmail.com>
2021-11-30 17:23:15 +0545

    fixed the issue of wishlist

--- lib/provider/wishlist_provider.dart
+++ lib/provider/wishlist_provider.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:food_delivery/models/wishlist_model.dart';
 
 class WishListProvider with ChangeNotifier {
   void addWishListData({
@@ -21,6 +22,46 @@ class WishListProvider with ChangeNotifier {
       'wishListImage': wishListImage,
       'wishListPrice': wishListPrice,
       'wishListQuantity': wishListQuantity,
+      'wishList': true,
     });
+  }
+
+  // Get WishList Products
+  List<WishListModel> wishLstItems = [];
+
+  void getWishListProduct() async {
+    List<WishListModel> newList = [];
+    QuerySnapshot data = await FirebaseFirestore.instance
+        .collection('wishListData')
+        .doc(FirebaseAuth.instance.currentUser?.uid)
+        .collection('CustomerWishList')
+        .get();
+
+    for (var element in data.docs) {
+      WishListModel wishListModel = WishListModel(
+        wishListId: element.get('wishListId'),
+        wishListName: element.get('wishListName'),
+        wishListImage: element.get('wishListImage'),
+        wishListPrice: element.get('wishListPrice'),
+      );
+      newList.add(wishListModel);
+    }
+    wishLstItems = newList;
+    notifyListeners();
+  }
+
+  List<WishListModel> get getWishListProducts {
+    return wishLstItems;
+  }
+
+  // Delete Wishlist Products
+  void deleteWishListProducts(wishListId) {
+    FirebaseFirestore.instance
+        .collection('wishListData')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .collection('CustomerWishList')
+        .doc(wishListId)
+        .delete();
+    notifyListeners();
   }
 }
--- lib/screens/wishlist_screen.dart
+++ lib/screens/wishlist_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/models/wishlist_model.dart';
+import 'package:food_delivery/provider/wishlist_provider.dart';
+import 'package:food_delivery/utils/utils.dart';
+import 'package:provider/provider.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class WishListScrenn extends StatelessWidget {
@@ -6,9 +10,99 @@ class WishListScrenn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    WishListProvider wishListProvider = Provider.of<WishListProvider>(context);
+    wishListProvider.getWishListProduct();
     return Scaffold(
       appBar: AppBar(
-        title: 'My WishList'.text.make(),
+        title: 'My Wishlist'.text.make(),
+      ),
+      body: wishListProvider.getWishListProducts.isEmpty
+          ? Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/empty_wishlist.png',
+                  fit: BoxFit.contain,
+                  width: 100,
+                  height: 100,
+                ).centered(),
+              ],
+            )
+          : ListView.builder(
+              itemCount: wishListProvider.getWishListProducts.length,
+              itemBuilder: (context, index) {
+                WishListModel data =
+                    wishListProvider.getWishListProducts[index];
+                // ignore: avoid_print
+                print(data);
+                return Column(
+                  children: [
+                    WishListItems(
+                      productId: data.wishListId.toString(),
+                      productName: data.wishListName.toString(),
+                      productImageUrl: data.wishListImage.toString(),
+                      productPrice: data.wishListPrice.toString(),
+                    ),
+                  ],
+                );
+              }),
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class WishListItems extends StatefulWidget {
+  const WishListItems({
+    Key? key,
+    required this.productId,
+    required this.productName,
+    required this.productImageUrl,
+    required this.productPrice,
+  }) : super(key: key);
+
+  final String productId;
+  final String productName;
+  final String productImageUrl;
+  final String productPrice;
+
+  @override
+  State<WishListItems> createState() => _WishListItemsState();
+}
+
+class _WishListItemsState extends State<WishListItems> {
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Image.network(widget.productImageUrl),
+      title: widget.productName.text.make(),
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          '${widget.productPrice}\$/50 gm'.text.make(),
+          IconButton(
+            onPressed: () {
+              Utils.showMessage(
+                  context,
+                  "Food Delivery",
+                  'Remove Item From Your Wishlist?',
+                  'Yes',
+                  () {
+                    Provider.of<WishListProvider>(context, listen: false)
+                        .deleteWishListProducts(
+                      widget.productId,
+                    );
+                    Navigator.of(context).pop();
+                  },
+                  buttonText2: 'No',
+                  isConfirmDialog: true,
+                  onPressed2: () {
+                    Navigator.of(context).pop();
+                  });
+            },
+            icon: const Icon(Icons.delete),
+          ),
+        ],
       ),
     );
   }
